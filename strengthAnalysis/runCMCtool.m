e3d7c201ca236b2b4f11a82cc3529b5989411430
james Dunne <james.dunne@stanford.edu>
2014-08-13 15:04:19 -0700

    fixing logic
    fixing the search method logic

--- strengthAnalysis/runCMCtool.m
+++ strengthAnalysis/runCMCtool.m
@@ -1,8 +1,8 @@
-function [torqueDta coordData] = runCMCtool(mStrength, pathname, filein)
+function [torqueDta coordData] = runCMCtool(mStrength, pathname,resultsFolder, filein , modelFilePath)
 
     import org.opensim.modeling.*      % Import OpenSim Libraries
 
-    cmcTool = CMCTool(fullfile(pathname,filein));
+    cmcTool = CMCTool(fullfile(pathname,resultsFolder, filein));
 
 %% set the path's for some variables
 
@@ -10,8 +10,7 @@ function [torqueDta coordData] = runCMCtool(mStrength, pathname, filein)
     tasksPath               = fullfile(pathname,'gait2392_CMC_Tasks.xml' );
     externalLoadFilePath    = fullfile(pathname,'subject01_walk1_grf.xml');
     coordinatesFilePath     = fullfile(pathname,'ResultsRRA', 'subject01_walk1_RRA_Kinematics_q.sto' );
-    modelFilePath           = fullfile(pathname,'ResultsCMC', ['myModel_' num2str(mStrength) '.osim']);
-    resultsPath             = fullfile(pathname,'ResultsCMC', ['testRun_' num2str(mStrength)]);
+    resultsPath             = fullfile(pathname,resultsFolder, ['testRun_' num2str(mStrength)]);
     
 %% Change the output folder path from CMCSetup
 
@@ -25,16 +24,16 @@ function [torqueDta coordData] = runCMCtool(mStrength, pathname, filein)
     cmcTool.setExternalLoadsFileName(externalLoadFilePath);
     cmcTool.setName('subject01')
     % Save the settings in a setup file
-    cmcTool.print( fullfile(pathname, filein) );
-    cmcTool = CMCTool(fullfile(pathname,filein));
+    cmcTool.print( fullfile(pathname, resultsFolder, filein));
+    cmcTool = CMCTool(fullfile(pathname, resultsFolder, filein));
 
 %% run cmc
     cmcTool.run()
 
 %% read in results and save to q and t
 
-    torqueDta = importdata( fullfile( pathname, 'ResultsCMC', ['testRun_' num2str(mStrength)], 'subject01_Actuation_force.sto'));
-    coordData = importdata( fullfile( pathname, 'ResultsCMC', ['testRun_' num2str(mStrength)], 'subject01_Kinematics_q.sto'   ));
+    torqueDta = importdata( fullfile( pathname, resultsFolder, ['testRun_' num2str(mStrength)], 'subject01_Actuation_force.sto'));
+    coordData = importdata( fullfile( pathname, resultsFolder, ['testRun_' num2str(mStrength)], 'subject01_Kinematics_q.sto'   ));
 
 %% Print the Model to the cmc folder
 
--- strengthAnalysis/strengthTester.m
+++ strengthAnalysis/strengthTester.m
@@ -21,12 +21,15 @@ muscles = {'vas_med_r' 'rect_fem_r' 'vas_int_r' 'vas_lat_r'};
 
 
 excludeList = {'subtalar_angle_r_reserve' 'mtp_angle_r_reserve' 'subtalar_angle_l_reserve' 'mtp_angle_l_reserve'};
+resultsFolder = 'ResultsCMC';
+workingDir = cd
 
 %% get the path to the model and cmc setup file
 [filein1, pathname] = uigetfile({'*.osim','osim'}, 'OSIM model file...');
 cd(pathname)
 [filein2, pathname2] = uigetfile({'*.xml','xml'}, 'CMC setup file...');
 [filein3, pathname3] = uigetfile({'*.sto','sto'}, 'ID results file...');
+cd(workingDir);
 
 % Model
 myModel = Model(fullfile(pathname,filein1));
@@ -36,58 +39,84 @@ cmcTool = CMCTool(fullfile(pathname2,filein2));
 % Inverse Dynamics
 m = importdata(fullfile(pathname3,filein3));
 
-%%
+%% run the 100% trial
 for ii = 1 : length(muscles)
     % change the musle strength of the model
     a(ii) = myModel.getMuscles.get( muscles{ii} ).getMaxIsometricForce ;
 end
 
-    myModel.print(fullfile(pathname,'ResultsCMC', ['myModel_' num2str(a) '.osim']) )
-
+    myModel.print(fullfile(pathname, resultsFolder, ['myModel_' num2str(100) '.osim']) );
+    cmcTool.print( fullfile(pathname, resultsFolder, filein2 ));
+    modelFilePath = fullfile(pathname,pathname);
+    % Initial CMC run
+    [t q] = runCMCtool(100, pathname, [] ,filein2 );
+    
     b = a/2;
     c = a;
     % run the max and min CMC results
-    [t q] = runCMCtool(100, pathname, filein2);
+    % [t q] = runCMCtool(100, pathname,resultsFolder, filein2);
 
-    stepsize = 1;
-    satisfyQs  = 1;
-    satisfyTs  = 1;
-    
-    while stepsize > 1 
-        
-        currentStepSize = a/2;
-           
-        if satisfyQs == 0 && satisfyTs == 0
+%%
+maxValue = 100;
+lowestValue = 1;
+stepsize = 5;
+[currentValue]= midpoint(maxValue,lowestValue );
+nSteps = 1;
 
-            b = a + currentStepSize;
-            
-        elseif satisfyQs == 1 && satisfyTs == 1
 
-            b = a - currentStepSize;
+while stepsize > 1 
 
-        end
-        display( num2str(a) );
-        
-        % calculate the the step size
-        stepsize = mean( abs(a - b) );
-        % caluclate the percentage of max
-        mCapacity = (b(1)/c(1))*100;
-        % change the muscle strength of the model
+    if nSteps == 1
+       % run a 50% trial 
         for ii = 1 : length(muscles)
             myModel.getMuscles.get( muscles{ii} ).setMaxIsometricForce( b(ii) ) ;
         end
-        myModel.print(fullfile(pathname,'ResultsCMC', ['myModel_' num2str(b) '.osim']) );
-
+        myModel.print(fullfile(pathname,resultsFolder, ['myModel_' num2str(mCapacity) '.osim']) );
         % run CMC with new strength
-        [t_n q_n] = runCMCtool(mCapacity, pathname, filein2 );
+        [t_n q_n] = runCMCtool(50, pathname, resultsFolder ,filein2 );
         % do the comparisons
         [satisfyQs satisfyTs] = compareQsAndTs(q, m, q_n, t_n, excludeList);
-        
-        a = b;
-        currentStepSize = stepSize;
     end
-   
+    
+    
+    if satisfyQs == 1 && satisfyTs == 1
+        maxValue = currentValue;
+        currentValue = midpoint(currentValue, lowestValue );
+    elseif satisfyQs == 0 || satisfyTs == 0 
+        lowestValue  = currentValue;
+        currentValue = midpoint(maxValue,currentValue );
+    end
 
- load Handel 
- sound(y,Fs)
+
+    c = b*(currentValue/100);
+
+    % calculate the the step size
+    stepsize = mean( abs(b - c) );
+    % caluclate the percentage of max
+    mCapacity = round((c(1)/b(1))*100) ;
+
+    c = a*(currentValue/100);
+
+        % Change the muscle MaxIsoForce and run again
+        for ii = 1 : length(muscles)
+            myModel.getMuscles.get( muscles{ii} ).setMaxIsometricForce( b(ii) ) ;
+        end
+        myModel.print(fullfile(pathname,resultsFolder, ['myModel_' num2str(mCapacity) '.osim']) );
+        % run CMC with new strength
+        [t_n q_n] = runCMCtool(mCapacity, pathname, resultsFolder ,filein2 );
+        % do the comparisons
+        [satisfyQs satisfyTs] = compareQsAndTs(q, m, q_n, t_n, excludeList);
+
+    b = c;
+
+    display(['stepsize; ' num2str(stepsize) ' || mCapacity; ' num2str(mCapacity) ' || recFemStrength; ' num2str(b(1)) ]  );
+    nSteps = nSteps+1;
+    
+    if nSteps == 4
+        break
+    end
+end
+   
+% load Handel 
+%sound(y,Fs)
 %%

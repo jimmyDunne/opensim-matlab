a1ec93c8411bce3bec0f2301a46a944048896cc6
jimmyDunne <james.dunne@stanford.edu>
2017-02-22 11:25:00 -0800

    updates to changedir script

--- changedirectory.m
+++ changedirectory.m
@@ -1,36 +1,29 @@
-function changedirectory(name)
+function changedirectory(varargin)
 % Changes the Matlab current folder from input string
 
-if isempty(name)
-    error('need an input string')
+
+data = [{'opensim'} {'/Users/jimmy/repository/opensim-master/opensim-core/OpenSim/Sandbox/MatlabScripts'};...
+        {'walker'} {'/Users/jimmy/repository/opensim-master/opensim-core/Bindings/Java/Matlab/Dynamic_Walker_Example'};...
+        {'noise'} {'/Users/jimmy/repository/kinematicNoise'};...
+        {'robot'} {'/Users/jimmy/repository/robotData'};...
+        {'c3d'} {'/Users/jimmy/repository/c3d2OpenSim'};...
+        {'osim-matlab'} {'/Users/jimmy/repository/opensim-matlab'} ];
+    
+    
+    
+    
+if ~nargin
+    display(data(:,1))
+elseif nargin
+    name = char(varargin{1});
 end
 
-%% opensim-core sandbox/Matlab scripts directory
-if stcmp(name, 'opensim')
-    cd('/Users/jimmy/repository/opensim-master/opensim-core/OpenSim/Sandbox/MatlabScripts')
-end
 
-%% opensim-core dynamic walker directory
-if stcmp(name, 'walker')
-    cd('/Users/jimmy/repository/opensim-master/opensim-core/Bindings/Java/Matlab/Dynamic_Walker_Example')
-end
-
-%% noise data set
-if stcmp(name, 'noise')
-    cd('/Users/jimmy/repository/kinematicNoise')
-end
-
-%% robot data set
-if stcmp(name, 'robot')
-    cd('/Users/jimmy/repository/robotData')
-end
-
-%% scripts o converting c3d data to opensim
-if stcmp(name, 'c3d')
-    cd('/Users/jimmy/repository/c3d2OpenSim')
-end
-
-%% james dunnes scripts for opensim
-if stcmp(name, 'c3d')
-    cd('/Users/jimmy/repository/opensim-matlab')
+if strcmp(name, 'list')
+    display(data(:,1))
+else
+    
+    Index = find(not(cellfun('isempty', strfind(data(:,1), name))));
+    disp(['Changing working directory to ' char(data(Index, 2)) ]);
+    cd(char(data(Index, 2)))
 end

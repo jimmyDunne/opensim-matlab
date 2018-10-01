89d447a46990c96b46fa0879740c8859d9241fd2
jimmyDunne <james.dunne@stanford.edu>
2018-10-01 12:38:30 -0400

    updates

--- misc/batchIKCreation.m
+++
@@ -1,4 +0,0 @@
-[struct root] = xml_read('../Subject03/hpl_03_IK_Setup_Walk_100_01.xml');
-
-struct.IKTool.IKTrialSet.objects.IKTrial.marker_file = 'Walk_100 02.trc';
-xml_write('../Subject03/hpl_03_IK_Setup_Walk_100_02.xml',struct,root);
